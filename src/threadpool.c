dd9844e7d6d1c92cb768bc0a3f94a640204e567e
Livio Conzett <5398351+LivioConzett@users.noreply.github.com>
2026-03-29 20:18:39 +0200

    trying to get it to run on the lowlevel server

--- src/threadpool.c
+++ src/threadpool.c
@@ -4,6 +4,11 @@
 #include <unistd.h>
 //#include <threadpool.h>
 
+#ifndef THREADPOOL_H
+#define THREADPOOL_H
+
+#include <pthread.h>
+
 #define THREADS 16
 #define QUEUE_SIZE 100
 
@@ -23,10 +28,13 @@ typedef struct {
   int stop;
 } threadpool_t;
 
+// Function declarations
 void threadpool_init(threadpool_t* pool);
 void threadpool_destroy(threadpool_t* pool);
 void threadpool_add_task(threadpool_t* pool, void (*function)(void*), void* arg);
 void example_task(void* arg);
+
+#endif // THREADPOOL_H 
 
 
 
@@ -34,7 +42,7 @@ void example_task(void* arg);
 void* thread_function(void* threadpool) {
     threadpool_t* pool = (threadpool_t*)threadpool;
     printf("starting thread\n");
-    
+
     while (1) {
         pthread_mutex_lock(&(pool->lock));
 
