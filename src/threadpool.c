57c52f47566bd30528b0e00e3436a51eca10a9a9
Livio Conzett <5398351+LivioConzett@users.noreply.github.com>
2026-03-29 19:15:12 +0200

    addd the threadpool destroy function

--- src/threadpool.c
+++ src/threadpool.c
@@ -11,13 +11,30 @@ void *thread_function(void *arg){
 
     threadpool_t *pool = (threadpool_t*) arg;
 
-    while(!pool->stop){
+    while(1){
 
         pthread_mutex_lock(&(pool->lock));
 
-        pthread_cond_wait(&(pool->notify), &(pool->lock));
+        while(pool->queued == 0 && !pool->stop){
+            pthread_cond_wait(&(pool->notify), &(pool->lock));
+        }
+
+        if(pool->stop){
+            pthread_mutex_unlock(&(pool->lock));
+            pthread_exit(NULL);
+        }
+
+        task_t task = pool->task_queue[pool->queue_front];
+        pool->queue_front = (pool->queue_front + 1) % QUEUE_SIZE;
+        pool->queued--;
+
+        pthread_mutex_unlock(&(pool->lock));
+
+        (*(task.fn))(task.arg);
 
     }
+
+    return NULL;
 }
 
 
@@ -33,4 +50,21 @@ void threadpool_init(threadpool_t *pool){
     for(int i = 0; i < THREAD_POOL_SIZE; i++){
         pthread_create(&(pool->threads[i]), NULL, thread_function, pool);
     }
+}
+
+
+void threadpool_destroy(threadpool_t *pool){
+    pthread_mutex_lock(&(pool->lock));
+    pool->stop = 1;
+    pthread_cond_broadcast(&(pool->notify));
+    pthread_mutex_unlock(&(pool->lock));
+
+    for(int i = 0; i < THREAD_POOL_SIZE; i++){
+        pthread_join(pool->threads[i], NULL);
+    }
+
+    pthread_mutex_destroy(&(pool->lock));
+    pthread_cond_destroy(&(pool->notify));
+
+
 }
