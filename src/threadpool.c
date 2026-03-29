3cc241d068923cc8320a22f7a0f0704a0887fa3d
Livio Conzett <5398351+LivioConzett@users.noreply.github.com>
2026-03-29 19:18:36 +0200

    trying to get the lowlevel webhook to compile

--- src/threadpool.c
+++ src/threadpool.c
@@ -2,8 +2,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <threadpool.h>
+//#include <threadpool.h>
 
+#define THREADS 16
+#define QUEUE_SIZE 100
+
+typedef struct {
+  void (*fn)(void* arg);
+  void* arg;
+} task_t;
+
+typedef struct {
+  pthread_mutex_t lock;
+  pthread_cond_t notify;
+  pthread_t threads[THREADS];
+  task_t task_queue[QUEUE_SIZE];
+  int queued;
+  int queue_front;
+  int queue_back;
+  int stop;
+} threadpool_t;
 
 void *thread_function(void *arg){
 
