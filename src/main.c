5e33c6b478009122175445e1dbf04288501f0ff0
Livio Conzett <5398351+LivioConzett@users.noreply.github.com>
2026-03-29 19:59:18 +0200

    trying to get it to run on the lowlevel server

--- src/main.c
+++ src/main.c
@@ -7,18 +7,18 @@
 
 int main(int argc, char *argv[]){
 
-    threadpool_t pool;
-    threadpool_init(&pool);
+    // threadpool_t pool;
+    // threadpool_init(&pool);
 
-    for(int i = 0; i < 200; i++){
-        int* task_num = malloc(sizeof(int));
-        *task_num = i;
-        threadpool_add_task(&pool, example_task, task_num);
-    }
+    // for(int i = 0; i < 200; i++){
+    //     int* task_num = malloc(sizeof(int));
+    //     *task_num = i;
+    //     threadpool_add_task(&pool, example_task, task_num);
+    // }
 
-    sleep(20);
+    // sleep(20);
 
-    threadpool_destroy(&pool);
+    // threadpool_destroy(&pool);
 
     return 0;
 }
